f033f6fdab61343cf9ed3193216db38c73325584
Nator26-NT <111059939+Nator26-NT@users.noreply.github.com>
2022-09-26 12:54:04 +0200

    Add files via upload

---
+++ polygon_area_calculator.py
@@ -0,0 +1,44 @@
+print ("POLYGON AREA CALCULATOR")
+
+print("===========================")
+
+print("===========================")
+
+
+
+
+print("TRIANGLE____")
+
+print("===============================")
+
+base = float(input("Enter your triangle base___    "))
+height = float(input("Enter Your triangle height___   "))
+
+Triangle = (base*height/2)
+print(Triangle , "   Area of triangle !!")
+print("===============================")
+print("===============================")
+
+print("TRAPEZIUM____")
+
+base_1 = float(input("Enter your trapezium base___1    "))
+
+base_2 = float(input("Enter your trapezium base__2_    "))
+
+height_2 = float(input("Enter your trapezim height___2    "))
+
+Trapezium = (base_1 + base_2*height /2)
+
+print("This is the area of the Trapezium:-) " , Trapezium)
+
+print("===============================")
+print("===============================")
+
+Area_of_polygon = (Triangle + Trapezium)
+
+print("Area of polygon == " , Area_of_polygon)
+
+
+
+
+
